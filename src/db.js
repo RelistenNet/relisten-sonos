a0918f7d25c6e85cf78906f43901f4dfc06c1c16
Alec Gorge <alecgorge@gmail.com>
2017-06-30 23:51:14 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const winston = require('./logger');
 
-var connection = mysql.createConnection(process.env.DATABASE_URL || (process.env.MYSQL_URL_INT || 'mysql://root@127.0.0.1/relisten');
+var connection = mysql.createConnection(process.env.DATABASE_URL || process.env.MYSQL_URL_INT || 'mysql://root@127.0.0.1/relisten');
 
 connection.connect(function(err) {
   if (err) {
