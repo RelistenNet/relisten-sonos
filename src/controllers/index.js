b3437b25e5c48774e63bedb32bfa418b41592164
Daniel Saewitz <switz213@gmail.com>
2018-03-10 15:46:05 -0500

    add svg-to-png test code

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const router = express.Router()
+const fs = require('pn/fs');
+const svg2png = require('svg2png');
+
+const router = express.Router();
 
 router.get('/', (req, res) => {
   return res.json({ hi: 'hi world' });
+});
+
+router.get('/album-art', (req, res) => {
+  fs.readFile(__dirname + '/../../images/relisten-album-background.svg')
+    .then(svg2png)
+    .then(buffer => {
+      res.type('image/png');
+      res.send(buffer);
+    })
+    .catch(e => {
+      // just send a default placeholder image here I guess.
+      console.log(e)
+      return res.json({ error: true, e })
+    });
 });
 
 module.exports = router;
