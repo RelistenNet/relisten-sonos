1ddf852ac19d198a2f57c56f59f4f4985017d5ad
Daniel Saewitz <git@saewitz.com>
2024-11-19 15:52:18 -0500

    Fix

--- src/lib/artistsCache.js
+++ src/lib/artistsCache.js
@@ -1,7 +1,7 @@
 
 const artistsCache = {};
 
-fetch('https://api/relisten.net/api/v2/artists')
+fetch('https://api.relisten.net/api/v2/artists')
   .then((res) => res.json())
   .then((json) => json.map((artist) => (artistsCache[artist.slug] = artist)));
 
